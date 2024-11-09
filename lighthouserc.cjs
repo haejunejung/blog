48751225d65c9e85dea7b5433728fd4b609ff0c5
haejunejung <jhj9422@unist.ac.kr>
2024-11-09 16:33:32 +0900

    [feat]: Lighthouse MDX & Lighthouse CI, Github Actions

---
+++ lighthouserc.cjs
@@ -0,0 +1,23 @@
+// https://github.com/GoogleChrome/lighthouse-ci/blob/main/docs/configuration.md
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: "yarn run preview", // The command to run to start the server
+      startServerReadyPattern: "Local", // String pattern to listen for the server
+      numberOfRuns: 1, // The number of times to run Lighthouse
+      url: ["http://127.0.0.1:4173/blog"], // A URL to run Lighthouse on.
+      // isSinglePageApplication: true,
+      settings: {
+        preset: "desktop",
+      },
+    },
+    assert: {
+      preset: "lighthouse:recommended",
+    },
+    upload: {
+      target: "temporary-public-storage", // The type of target to upload the data to. If set to anything other than "lhci", some of the options will not apply.
+    },
+    server: {},
+    wizard: {},
+  },
+};
