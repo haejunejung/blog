1361fff3d227171be12df9a4cb454264c181cc3d
haejunejung <jhj9422@unist.ac.kr>
2024-11-09 18:56:13 +0900

    [fix]: Fix typo

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     collect: {
       startServerCommand: "yarn run preview", // The command to run to start the server
       startServerReadyPattern: "Local", // String pattern to listen for the server
-      numberOfRuns: 5, // The number of times to run Lighthouse
+      numberOfRuns: 1, // The number of times to run Lighthouse
       url: ["http://localhost:4173"], // A URL to run Lighthouse on.
     },
     assert: {},
