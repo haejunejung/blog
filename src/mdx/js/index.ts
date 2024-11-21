5cb895762d9ffa0f73b16d3dc94d04052cc2da79
haejunejung <jhj9422@unist.ac.kr>
2024-11-21 15:51:14 +0900

    [feat]: 이벤트 전파

--- src/mdx/js/index.ts
+++ src/mdx/js/index.ts
@@ -7,4 +7,10 @@ export const jsList: MDXItemProps[] = [
     path: "/js/scope.mdx",
     importMdx: () => import("./scope.mdx"),
   },
+  {
+    title: "Event Propagation",
+    summary: "이벤트 전파에 대해 공부했습니다.",
+    path: "/js/event-propagation.mdx",
+    importMdx: () => import("./event-propagation.mdx"),
+  },
 ];
