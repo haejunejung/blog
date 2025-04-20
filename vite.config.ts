0b5492b86c15737f7dc38c1c3ddfd32b8dc8a58a
haejunejung <jhj9422@unist.ac.kr>
2025-04-20 18:39:35 +0900

    Setup vitest, tsd for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { resolve } from "node:path";
 import mdx from "@mdx-js/rollup";
 import react from "@vitejs/plugin-react";
@@ -44,4 +46,9 @@ export default defineConfig({
 			"@": resolve(__dirname, "src"),
 		},
 	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./setupTests.ts",
+	}
 });
