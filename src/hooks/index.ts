5f1565089254d5a4acdb16d55edbb30fa4a70012
haejunejung <jhj9422@unist.ac.kr>
2025-04-25 18:42:51 +0900

    Add useAsyncEffect that including AbortController

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { useNetworkStatus } from "./useNetworkStatus";
 export { usePreservedCallback } from "./usePreservedCallback";
+export { useAsyncEffect } from "./useAsyncEffect";
---
+++ src/hooks/useAsyncEffect.ts
@@ -0,0 +1,26 @@
+import type { ResolveCallable } from "@/types";
+import { type DependencyList, useEffect } from "react";
+
+export const useAsyncEffect = (
+	effect: (signal: AbortSignal) => Promise<ResolveCallable<void>>,
+	deps?: DependencyList,
+) => {
+	// biome-ignore lint/correctness/useExhaustiveDependencies: deps
+	useEffect(() => {
+		const abortController = new AbortController();
+		const { signal } = abortController;
+
+		let cleanup: ResolveCallable<void>;
+
+		effect(signal).then((result) => {
+			if (!signal.aborted) {
+				cleanup = result;
+			}
+		});
+
+		return () => {
+			abortController.abort();
+			cleanup?.();
+		};
+	}, [deps, effect]);
+};
